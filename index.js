eca8655ea3bc150cdd1c17dfbfcf64cbfe2edc01
peterbujaky <peterbujaky@gmail.com>
2023-01-12 21:01:49 +0100

    Fetch API & Manipulate the DOM

---
+++ index.js
@@ -0,0 +1,9 @@
+fetch("https://api.adviceslip.com/advice")
+.then(res => res.json())
+.then (data => {
+    const advice = data.slip.advice;
+    const id = data.slip.id;
+    document.getElementById("adviceText").innerHTML = advice;
+    document.getElementById("adviceID").innerHTML = `Advice #${id}`;
+})
+
