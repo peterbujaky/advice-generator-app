0f74c5c89cb4db9480e172a3a757f4faef809851
peterbujaky <peterbujaky@gmail.com>
2023-01-12 21:41:51 +0100

    Add basic styling

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ fetch("https://api.adviceslip.com/advice")
 .then (data => {
     const advice = data.slip.advice;
     const id = data.slip.id;
-    document.getElementById("adviceText").innerHTML = advice;
+    document.getElementById("adviceText").innerHTML = `"${advice}"`;
     document.getElementById("adviceID").innerHTML = `Advice #${id}`;
 })
 
