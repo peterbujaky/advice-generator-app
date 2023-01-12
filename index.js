8e9ec67ecd9be949d6b35d83a3ea255beff66ecb
peterbujaky <peterbujaky@gmail.com>
2023-01-12 23:11:24 +0100

    Add buttons and new API fetch

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 fetch("https://api.adviceslip.com/advice")
 .then(res => res.json())
 .then (data => {
-    const advice = data.slip.advice;
-    const id = data.slip.id;
+    let advice = data.slip.advice;
+    let id = data.slip.id;
     document.getElementById("adviceText").innerHTML = `"${advice}"`;
     document.getElementById("adviceID").innerHTML = `Advice #${id}`;
+})
+
+document.getElementById("button").addEventListener("click", () => {
+    fetch("https://api.adviceslip.com/advice")
+    .then(res => res.json())
+    .then(data => {
+        let advice = data.slip.advice;
+        let id = data.slip.id;
+        document.getElementById("adviceText").innerHTML = `"${advice}"`;
+        document.getElementById("adviceID").innerHTML = `Advice #${id}`;
+    })
 })
 
